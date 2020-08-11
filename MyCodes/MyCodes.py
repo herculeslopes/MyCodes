e33c4dc10df9bb5921c16dcf5786889ea130fde5
herculeslopes <0herculeslopes0@gmail.com>
2020-08-11 18:56:39 -0300

    update

--- MyCodes/MyCodes.py
+++ MyCodes/MyCodes.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from PIL import ImageTk, Image
 from time import sleep
 from tkinter import filedialog
+from tkinter.font import Font
 import sqlite3
 
 class MainProgram():
@@ -12,11 +13,16 @@ class MainProgram():
         self.root.resizable(False, False)
         self.root.configure(background='#121212')
 
+        self.LabelFont = Font(family='Arial', size=30)
+
         self.ChooseProfile()
 
     
-    def CreateImage(self, ):
+    def CreateImage(self, path):
+        ImageFile = Image.open(path)
+        TkImage = ImageTk.PhotoImage(ImageFile)
 
+        return TkImage
 
 
     def ChooseProfile(self):
@@ -50,7 +56,7 @@ class MainProgram():
 
                     ProfileImage = ImageTk.PhotoImage(ImageFile.resize((250, 250), Image.ANTIALIAS))
 
-                ProfileButton = tk.Button(ProfileFrame, image=ProfileImage, bg='#121212', activebackground='#121212', bd=0, relief=tk.FLAT, command=lambda a = Button_id: Login(a))
+                ProfileButton = tk.Button(ProfileFrame, image=ProfileImage, bg='#121212', activebackground='#121212', bd=0, relief=tk.FLAT, command=lambda a = Button_id: self.Login(a))
                 ProfileButton.image = ProfileImage
                 ProfileButton.pack(side=tk.LEFT, padx=10)
         
@@ -85,27 +91,72 @@ class MainProgram():
 
 
     def CreateProfile(self):
+        def AddUsername():
+            self.CleanMainWindow()
+
+            
+
         pass
 
 
-    def Login(self):
+    def Login(self, Button_id):
         self.CleanMainWindow()
 
         self.DB_Cursor.execute(f'''SELECT imagepath, username FROM Profile WHERE id = {Button_id}''')
-        data = self.DB_Cursor.fetchone()
+        Data = self.DB_Cursor.fetchone()
 
         UpFrame = tk.Frame(self.root, bg='#121212', bd=0)
         UpFrame.pack(side=tk.TOP, anchor='w', padx=10, pady=10)
 
-        ImageFile = Image.open(data[0])
+        ImageFile = Image.open(Data[0])
         IconFile = ImageFile.resize((75, 75), Image.ANTIALIAS)
         IconImage = ImageTk.PhotoImage(IconFile)
-        IconButton = tk.Button(UpFrame, image=IconImage, bg='#121212', activebackground='#121212', bd=0, relief=tk.FLAT, command=ChooseProfile)
+        IconButton = tk.Button(UpFrame, image=IconImage, bg='#121212', activebackground='#121212', bd=0, relief=tk.FLAT, command=self.ChooseProfile)
         IconButton.image = IconImage
         IconButton.pack(side=tk.LEFT)
 
-        EditImage = self.CreateImage()
+        EditImage = self.CreateImage('Icons/edit.png')
+        EditButton = tk.Button(UpFrame, image=EditImage, bg='#121212', activebackground='#121212', bd=0, command=self.EditProfile)
+        EditButton.image = EditImage
+        EditButton.pack(side=tk.RIGHT, padx=5)
 
+        Username = Data[1]
+        UsernameLabel = tk.Label(self.root, text=Username, font=self.LabelFont, bg='#121212', fg='#999999')
+        UsernameLabel.pack(pady=(30, 5))
+
+        PasswordEntry = tk.Entry(self.root, font=self.LabelFont, bg='#333333', fg='#999999', bd=0, justify=tk.CENTER, show='•')
+        PasswordEntry.pack(pady=5)
+
+        def Submit(Button_id):
+            from os import system
+
+            self.DB_Cursor.execute(f'''SELECT username, password, imagepath FROM Profile WHERE id = {Button_id}''')
+            Data = self.DB_Cursor.fetchone()
+
+            Password = Data[1]
+
+            PasswordGiven = PasswordEntry.get()
+
+            if PasswordGiven == Password:
+                self.root.destroy()
+
+                system(f'''python MainWindow.py "{Data[0]}" "{Data[1]}" "{Data[2]}" {Button_id}''')
+
+            else:
+                PasswordEntry.delete(0, tk.END)
+
+
+        SubmitImage = self.CreateImage('Icons/submit.png')
+        SubmitButton = tk.Button(self.root, image=SubmitImage, bg='#121212', activebackground='#121212', bd=0, relief=tk.FLAT, command=lambda: Submit(Button_id))
+        SubmitButton.image = SubmitImage
+        SubmitButton.pack(pady=5)
+
+
+        self.root.bind('<Return>', lambda event: Submit(Button_id))
+
+
+    def EditProfile(self):
+        pass
 
 
 def Main():
